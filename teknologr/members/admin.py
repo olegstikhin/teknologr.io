e2ba7f4bf1dd9bf45a12f12c81a5be5187f54339
Walliski <walliski@gmail.com>
2016-04-28 19:29:01 +0300

    Show some tables in admin interface

--- teknologr/members/admin.py
+++ teknologr/members/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from members.models import *
 
-# Register your models here.
+admin.site.register(Members)
+admin.site.register(Committees)
+admin.site.register(Departments)
+
