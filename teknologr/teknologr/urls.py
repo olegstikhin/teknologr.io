3a4459edfedc726025d66696c15e7d17fde5ab9e
Thomas L <tlangens@gmail.com>
2017-01-05 02:03:19 +0200

    Add a django frontend base

--- teknologr/teknologr/urls.py
+++ teknologr/teknologr/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('api.urls')),
+    url(r'^', include('members.urls')),
+    url(r'^api/', include('api.urls')),
 ]
