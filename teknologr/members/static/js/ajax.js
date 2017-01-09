edb166eb95d341efcee80ba624096bb0970f1f25
Thomas L <tlangens@gmail.com>
2017-01-09 20:02:15 +0200

    removed unnecessary functions

--- teknologr/members/static/js/ajax.js
+++ teknologr/members/static/js/ajax.js
@@ -1,26 +1,10 @@
-function loadMember(id) {
-	$("#main").load("member/"+id, function(response, status, xhr){
+function loadMain(category, id) {
+	$("#main").load(category + '/' + id, function(response, status, xhr){
 		if (status == "error") {
-			$("#main").html('<h1 class="page-header">404 - Hittades inte</h1><h3>Denna medlem hittades inte i registret.</h3>');
+			$("#main").html('<h1 class="page-header">404 - Hittades inte</h1><h3>Det begärda objektet hittades inte</h3>');
 		}
 	});
 };
-
-function loadGroup(id) {
-	$("#main").load("TODO");
-};
-
-function loadFunctionary(id) {
-	$("#main").load("TODO");
-};
-
-function loadDecoration(id) {
-	$("#main").load("TODO");
-};
-
-function loadMain(category, id) {
-	$("#main").load(category + '/' + id)
-}
 
 function loadSide() {
 	var category = $('input[type=radio][name=side]:checked').val();
