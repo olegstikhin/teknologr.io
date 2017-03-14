d0e625ff77ed905b1b120568c87ca32fa92c0020
Morakh <jussi.paakkola@aalto.fi>
2017-03-14 12:43:30 +0200

    Refactor AJAX search to use member model methods

--- teknologr/members/lookups.py
+++ teknologr/members/lookups.py
@@ -19,19 +19,19 @@ class MemberLookup(LookupChannel):
             return [] # No words in query (only spaces?)
 
 
-        return Member.objects.filter(*args)
+        return Member.objects.filter(*args).order_by('surname', 'given_names')[:10]
 
     def get_result(self, obj):
         """ result is the simple text that is the completion of what the person typed """
-        return "%s %s" % (escape(obj.surname), escape(obj.given_names))
+        return obj._get_full_name()
 
     def format_match(self, obj):
         """ (HTML) formatted item for display in the dropdown """
-        return "%s %s" % (escape(obj.surname), escape(obj.given_names))
+        return obj._get_full_name()
 
     def format_item_display(self, obj):
         """ (HTML) formatted item for displaying item in the selected deck area """
-        return "%s %s" % (escape(obj.surname), escape(obj.given_names))
+        return obj._get_full_name()
 
     def check_auth(self, request):
         #TODO: Actual authentication?
