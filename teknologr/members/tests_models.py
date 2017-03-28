b379d70b41d77cc10080527e80e3684cc0061d10
Walliski <walliski@gmail.com>
2017-03-28 11:45:27 +0300

    Add DecorationOwnerShip unit test

--- teknologr/members/tests_models.py
+++ teknologr/members/tests_models.py
@@ -1,5 +1,7 @@
+import datetime
+
 from django.test import TestCase
-from members.models import Member
+from members.models import *
 
 class MemberTest(TestCase):
     def setUp(self):
@@ -16,3 +18,13 @@ class MemberTest(TestCase):
     def test_str(self):
         foobar = Member.objects.get(given_names="Foo Bar", surname="Tester")
         self.assertEqual(str(foobar), "Foo Bar Tester")
+
+class DecorationOwnerShipTest(TestCase):
+    def setUp(self):
+        member = Member.objects.create(given_names="Foo Bar", preferred_name="Foo", surname="Tester")
+        decoration = Decoration.objects.create(name="Test Decoration")
+        DecorationOwnership.objects.create(member=member, decoration=decoration, acquired=datetime.date.today())
+
+    def test_str(self):
+        dec_ownership = DecorationOwnership.objects.get(pk=1)
+        self.assertEqual(str(dec_ownership), "Test Decoration - Foo Bar Tester")
