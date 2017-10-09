ccef676627cb67183a8c27b688c05482846d6f14
Morakh <jussi.paakkola@aalto.fi>
2017-10-09 17:16:34 +0300

    Add sendMailUtils

---
+++ teknologr/api/mailutils.py
@@ -0,0 +1,25 @@
+from django.core.mail import send_mail
+
+def mailNewPassword(member, password, sender="infochef@tf.fi"):
+
+	subject = 'Ditt nya TF-lösenord'
+
+	message = '''Hej,
+
+	Här är ditt nya TF-lösenord:
+
+	%s
+
+	Vänligen logga in på TF:s hemsida och byt lösenordet så snabbt som möjligt.
+
+	Ifall du har problem eller frågor kontakta %s
+	''' % (password, sender)
+
+	reciever = member.email
+
+	return send_mail(
+		subject,
+		message,
+		sender,
+		[reciever],
+		fail_silently=False)
