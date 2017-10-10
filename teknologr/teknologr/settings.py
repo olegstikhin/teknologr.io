d6ca8e18eeb642dfb080ea8d1d570e63b67d196a
Morakh <jussi.paakkola@aalto.fi>
2017-10-10 12:30:34 +0300

    Add settings.py variables for mail sending

--- teknologr/teknologr/settings.py
+++ teknologr/teknologr/settings.py
@@ -227,3 +227,10 @@ AUTH_LDAP_GLOBAL_OPTIONS = {
 # Confs nexsecary for ajax-select
 AJAX_SELECT_INLINES = 'staticfiles'
 STATIC_ROOT = env("STATIC_ROOT", None)
+
+# Email conf
+EMAIL_HOST = env("EMAIL_HOST", "smtp.gmail.com")
+EMAIL_PORT = env("EMAIL_PORT", 587)"587"
+EMAIL_HOST_USER = env("EMAIL_HOST_USER", None)
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", None)
+EMAIL_USE_TLS = env("EMAIL_USE_TLS", True)
