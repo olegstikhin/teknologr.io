ff53b5eef9836b0c3f99b79346741209941460aa
Oleg Stikhin <oleg.stikhin@aalto.fi>
2018-02-05 15:17:39 +0100

    PEP 8 compliance

--- teknologr/api/mailutils.py
+++ teknologr/api/mailutils.py
@@ -1,28 +1,29 @@
 from django.core.mail import send_mail
 
+
 def mailNewPassword(member, password, sender="infochef@tf.fi"):
 
-	subject = 'Ditt nya TF-lösenord'
+    subject = 'Ditt nya TF-lösenord'
 
-	message = '''Hej,
+    message = '''Hej,
 
-	Här är ditt nya TF-lösenord:
+    Här är ditt nya TF-lösenord:
 
-	%s
+    %s
 
-	Vänligen logga in på TF:s hemsida och byt lösenordet så snabbt som möjligt:
-	https://medlem.teknologforeningen.fi/index.php/aendra-loesenord
+    Vänligen logga in på TF:s hemsida och byt lösenordet så snabbt som möjligt:
+    https://medlem.teknologforeningen.fi/index.php/aendra-loesenord
 
-	Vid frågor eller ifall du inte begärt detta, kontakta %s
+    Vid frågor eller ifall du inte begärt detta, kontakta %s
 
-	Detta är ett automatiskt meddelande, du behöver inte svara på det.
-	''' % (password, sender)
+    Detta är ett automatiskt meddelande, du behöver inte svara på det.
+    ''' % (password, sender)
 
-	reciever = member.email
+    reciever = member.email
 
-	return send_mail(
-		subject,
-		message,
-		sender,
-		[reciever],
-		fail_silently=False)
+    return send_mail(
+        subject,
+        message,
+        sender,
+        [reciever],
+        fail_silently=False)
